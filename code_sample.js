6bf5930fd7e753f5eee2afbf43fc9d37b061eb1b
Diogo Costa <55318635+diogo-costa98@users.noreply.github.com>
2023-02-16 13:09:46 +0000

    Update code_sample.js

--- code_sample.js
+++ code_sample.js
@@ -4,20 +4,20 @@ const redis = new Redis();
 
 const resolver = async (parent, args, context, info) => {
   const { id } = args;
-  const key = `product:${id}`;
+  const key = `question:${id}`;
 
-  // Try to get the data from Redis cache
-  let product = await redis.get(key);
+  // Try to get the question data from Redis cache
+  let question = await redis.get(key);
   
   // If not found, fetch it from the database
-  if (!product) {
-    product = await Product.findById(id);
+  if (!question) {
+    product = await Question.findById(id);
 
     // Store the result in Redis cache with an expiration time of 1 hour
-    await redis.set(key, JSON.stringify(product), 'EX', 60 * 60);
+    await redis.set(key, JSON.stringify(question), 'EX', 60 * 60);
   } else {
-    product = JSON.parse(product);
+    question = JSON.parse(question);
   }
 
-  return product;
+  return question;
 };
